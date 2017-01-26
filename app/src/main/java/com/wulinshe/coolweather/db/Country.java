c288f699081d6f4699073801ba8bede1a2fdd212
bingzi <dz6168@163.com>
2017-01-26 22:49:34 +0800

    dj

---
+++ app/src/main/java/com/wulinshe/coolweather/db/Country.java
@@ -0,0 +1,51 @@
+package com.wulinshe.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/1/24.
+ * countryName 县的名字；
+ * weatherId对应天气的id；
+ * 
+ * 
+ */
+
+public class Country extends DataSupport {
+    private  int id;
+    private  String countryName;
+    private int weatherId;
+    private int cityId;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getcityId() {
+        return  cityId;
+    }
+
+    public void setcityId(int cityId) {
+        this. cityId = cityId;
+    }
+}
